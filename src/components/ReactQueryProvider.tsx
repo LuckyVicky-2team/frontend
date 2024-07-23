4bb6ebb5523c436a04d1a3174ce43a256482e626
Jin-Chanyong <cksdyd324@gmail.com>
2024-07-23 17:49:42 +0900

    feat: 리액트 쿼리 설정

---
+++ src/components/ReactQueryProvider.tsx
@@ -0,0 +1,20 @@
+import React, { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000,
+        },
+    },
+});
+
+export default function ReactQueryProvider({ children }: PropsWithChildren) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+}

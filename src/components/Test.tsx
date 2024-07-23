2d21c3627e3cb4156d459b2de841152c4265f622
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-07-23 15:28:58 +0900

    feat:webhook action test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Test = () => {
-    return <div>Test</div>;
+    return (
+        <div>
+            <p>discord - webhook action test</p>
+        </div>
+    );
 };
 
 export default Test;

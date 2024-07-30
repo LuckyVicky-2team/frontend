03147a54de88b67aba06d103dc5d129b7c9c9b8b
Songeun <hannahkeeien@gmail.com>
2024-07-30 16:48:32 +0900

    feat: modal 구조 설계

--- src/app/test/song/index.tsx
+++ src/app/test/song/index.tsx
@@ -1,4 +1,22 @@
-function songeunPage() {
-  return <div>songeunPage</div>;
+import { useState } from 'react';
+
+function SongeunPage() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModalOutsideClick = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div>
+      {modalOpen && (
+        <div
+          style={{ backgroundColor: 'rgba(0,0,0,0.3)' }}
+          onClick={handleModalOutsideClick}>
+          <div style={{ backgroundColor: 'white' }}>Modal</div>
+        </div>
+      )}
+    </div>
+  );
 }
-export default songeunPage;
+export default SongeunPage;

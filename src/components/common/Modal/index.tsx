bf937fbe69e78b50a2b3d56b06342dccb6a7dd17
Songeun <hannahkeeien@gmail.com>
2024-07-30 17:27:37 +0900

    feat: Modal 컴포넌트 분리

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,4 +1,39 @@
-function Modal() {
-  return <div>Modal</div>;
+interface IModalProps {
+  modalOpen: boolean;
+  onClose: () => void;
+}
+
+function Modal({ modalOpen, onClose }: IModalProps) {
+  return (
+    <>
+      {modalOpen && (
+        <div
+          style={{
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            position: 'fixed',
+            top: '0',
+            bottom: '0',
+            left: '0',
+            right: '0',
+          }}
+          onClick={onClose}>
+          <div
+            onClick={e => {
+              e.stopPropagation();
+            }}
+            style={{
+              backgroundColor: 'white',
+              width: '100px',
+              height: '100px',
+            }}>
+            <button type="button" onClick={onClose}>
+              x
+            </button>
+            Modal Content
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 export default Modal;

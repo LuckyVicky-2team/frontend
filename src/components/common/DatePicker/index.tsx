92e5c75a27b4555e7358b2dd8a5cd2d6eca1e013
Songeun <hannahkeeien@gmail.com>
2024-07-31 16:39:46 +0900

    Fix: DatePicker가 모달 밖으로 빠져나갔을 때 hidden 되는 현상 수정

--- src/components/common/DatePicker/index.tsx
+++ src/components/common/DatePicker/index.tsx
@@ -1,6 +1,9 @@
 import { Control, Controller, FieldValues, Path } from 'react-hook-form';
-import ReactDatePicker from 'react-datepicker';
+import ReactDatePicker, { registerLocale } from 'react-datepicker';
+import { ko } from 'date-fns/locale/ko';
 import styles from './DatePicker.module.scss';
+import 'react-datepicker/dist/react-datepicker.css';
+import CalendarContainer from './CalenderContainer';
 
 interface IDatePickerProps<F extends FieldValues> {
   control: Control<F>;
@@ -20,14 +23,16 @@ function DatePicker<F extends FieldValues>({
   placeholder,
   className,
 }: IDatePickerProps<F>) {
+  registerLocale('ko', ko);
   return (
     <div className={`${styles.inputContainer} ${className}`}>
       <Controller
         control={control}
         name={name}
-        render={({ field: { value } }) => {
+        render={({ field: { value, ...fieldProps } }) => {
           return (
             <ReactDatePicker
+              {...fieldProps}
               id={id}
               locale="ko"
               dateFormat="yyyy.MM.dd HH:mm"
@@ -36,6 +41,7 @@ function DatePicker<F extends FieldValues>({
               selected={value}
               required={true}
               showTimeSelect
+              popperContainer={CalendarContainer}
             />
           );
         }}

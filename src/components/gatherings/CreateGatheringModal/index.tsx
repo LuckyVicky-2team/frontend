2397c90cae45182e45db8577c240373b5c538013
Songeun <hannahkeeien@gmail.com>
2024-07-31 14:31:24 +0900

    feat: 모임 생성 모달 form 구조 설계

--- src/components/gatherings/CreateGatheringModal/index.tsx
+++ src/components/gatherings/CreateGatheringModal/index.tsx
@@ -1,4 +1,17 @@
 import Modal from '@/components/common/Modal';
+import { FormProvider, useForm } from 'react-hook-form';
+import styles from './CreateGatheringModal.module.scss';
+
+interface CreateGatheringFormValues {
+  image: string;
+  tags: string;
+  title: string;
+  content: string;
+  location: string;
+  date: Date;
+  participants: number;
+  type: 'free' | 'accept';
+}
 
 interface ICreateGatheringModalProps {
   modalOpen: boolean;
@@ -9,9 +22,26 @@ function CreateGatheringModal({
   modalOpen,
   onClose,
 }: ICreateGatheringModalProps) {
+  const methods = useForm<CreateGatheringFormValues>({
+    mode: 'all',
+  });
+  const { register, handleSubmit } = methods;
+
+  const onSubmit = (gatheringInfo: CreateGatheringFormValues) => {
+    console.log({ gatheringInfo });
+  };
+
   return (
     <Modal modalOpen={modalOpen} onClose={onClose}>
-      ssss
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.inputArea}>
+            <div className={styles.inputContainer}>
+              <textarea id="content" {...register('content')} />
+            </div>
+          </div>
+        </form>
+      </FormProvider>
     </Modal>
   );
 }

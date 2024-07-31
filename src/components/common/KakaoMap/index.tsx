e494e1e43e5d59d4d739f8547d43958dbf68b285
Jin-Chanyong <cksdyd324@gmail.com>
2024-07-31 14:34:45 +0900

    Feat: geolocation api 함수 생성

--- src/components/common/KakaoMap/index.tsx
+++ src/components/common/KakaoMap/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import styles from './KakaoMap.module.scss';
 import { useEffect, useRef } from 'react';
+import styles from './KakaoMap.module.scss';
 
 declare global {
   // eslint-disable-next-line no-unused-vars
@@ -10,21 +10,28 @@ declare global {
   }
 }
 
-export default function KakaoMap() {
+interface IKakaoMapProps {
+  className?: string;
+}
+
+export default function KakaoMap({ className }: IKakaoMapProps) {
   const mapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     window.kakao.maps.load(() => {
       const options = {
-        //지도를 생성할 때 필요한 기본 옵션
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3, //지도의 레벨(확대, 축소 정도)
+        // 지도를 생성할 때 필요한 기본 옵션
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표.
+        level: 3, // 지도의 레벨(확대, 축소 정도)
       };
 
       // eslint-disable-next-line no-unused-vars
       const map = new window.kakao.maps.Map(mapRef.current, options); //지도 생성 및 객체 리턴
+
+      const zoomControl = new window.kakao.maps.ZoomControl(); // 지도 줌 컨트롤 바 선언
+      map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT); // 지도 줌 컨트롤 바 오른쪽에 생성
     });
   }, []);
 
-  return <div className={styles.map} ref={mapRef}></div>;
+  return <div className={`${styles.map} ${className}`} ref={mapRef}></div>;
 }

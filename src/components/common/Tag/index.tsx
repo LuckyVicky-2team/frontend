a0e1f473eddf8210a0011e240f37e8fa0f96f576
Songeun <hannahkeeien@gmail.com>
2024-07-31 11:15:30 +0900

    Chore: 사용 예시 추가

--- src/components/common/Tag/index.tsx
+++ src/components/common/Tag/index.tsx
@@ -2,19 +2,20 @@ import { ReactNode } from 'react';
 import styles from './Tag.module.scss';
 
 // 사용 예시
+//<Tag backgroundColor='#ffffaa' fontColor='#ffff00' size='small'>태그</Tag>
 
 interface ITagProps {
   children: ReactNode | undefined;
-  backgroundColor: string;
-  fontColor: string;
-  size: 'small' | 'medium' | 'large';
+  backgroundColor?: string;
+  fontColor?: string;
+  size?: 'small' | 'medium' | 'large';
 }
 
 function Tag({
   children,
   backgroundColor = '#ffffff',
   fontColor = '#000000',
-  size,
+  size = 'medium',
 }: ITagProps) {
   return (
     <div

1882019aca7a914dc8d1709b02b4ad641b1e08a3
Jin-Chanyong <cksdyd324@gmail.com>
2024-07-31 13:35:53 +0900

    Feat: 카카오지도 컴포넌트 생성

--- src/components/common/KakaoMap/index.tsx
+++ src/components/common/KakaoMap/index.tsx
@@ -1,5 +1,30 @@
+'use client';
+
 import styles from './KakaoMap.module.scss';
+import { useEffect, useRef } from 'react';
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface Window {
+    kakao: any;
+  }
+}
 
 export default function KakaoMap() {
-  return <div className={styles.map}></div>;
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    window.kakao.maps.load(() => {
+      const options = {
+        //지도를 생성할 때 필요한 기본 옵션
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        level: 3, //지도의 레벨(확대, 축소 정도)
+      };
+
+      // eslint-disable-next-line no-unused-vars
+      const map = new window.kakao.maps.Map(mapRef.current, options); //지도 생성 및 객체 리턴
+    });
+  }, []);
+
+  return <div className={styles.map} ref={mapRef}></div>;
 }

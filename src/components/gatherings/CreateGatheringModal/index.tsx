2c4ca03d945cd6df5282443a5cf4017ead8262b3
Songeun <hannahkeeien@gmail.com>
2024-07-31 14:42:18 +0900

    Feat: 모임 생성 form에 들어가는 내용 추가

--- src/components/gatherings/CreateGatheringModal/index.tsx
+++ src/components/gatherings/CreateGatheringModal/index.tsx
@@ -4,8 +4,8 @@ import styles from './CreateGatheringModal.module.scss';
 
 interface CreateGatheringFormValues {
   image: string;
-  tags: string;
   title: string;
+  tags: string;
   content: string;
   location: string;
   date: Date;
@@ -37,7 +37,36 @@ function CreateGatheringModal({
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputArea}>
             <div className={styles.inputContainer}>
+              <label htmlFor="image">이미지</label>
+              <input id="image" {...register} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="title">제목</label>
+              <input id="title" {...register} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="tags">태그</label>
+              <input id="tags" {...register} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="content">내용</label>
               <textarea id="content" {...register('content')} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="location">위치</label>
+              <input id="location" {...register} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="date">날짜</label>
+              <input id="date" {...register} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="title">인원</label>
+              <input id="title" {...register} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="type">모임 유형</label>
+              <input id="type" {...register} />
             </div>
           </div>
         </form>

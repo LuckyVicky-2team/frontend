ef980a58cb9f7ab70e936ae15d6ec1ec0e815cfa
Songeun <hannahkeeien@gmail.com>
2024-07-31 10:12:13 +0900

    refactor: styles css로 이동

--- src/app/test/song/page.tsx
+++ src/app/test/song/page.tsx
@@ -9,8 +9,10 @@ function SongeunPage() {
       style={{
         backgroundColor: 'white',
         height: '100vh',
+        zIndex: '1',
       }}>
-      <button type="button" onClick={handleModalOpen}>
+      <div style={{ zIndex: '10', backgroundColor: 'yellow' }}>zindex: 10</div>
+      <button type="button" onClick={handleModalOpen} style={{ zIndex: '2' }}>
         모달 클릭
       </button>
       <Modal modalOpen={modalOpen} onClose={handleModalClose} />
--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -9,30 +9,17 @@ function Modal({ modalOpen, onClose }: IModalProps) {
   return (
     <>
       {modalOpen && (
-        <div
-          style={{
-            backgroundColor: 'rgba(0,0,0,0.3)',
-            position: 'fixed',
-            top: '0',
-            bottom: '0',
-            left: '0',
-            right: '0',
-          }}
-          onClick={onClose}
-          className={styles.modalBackground}>
+        <div onClick={onClose} className={styles.modalBackground}>
           <div
             onClick={e => {
               e.stopPropagation();
             }}
-            style={{
-              backgroundColor: 'white',
-              width: '100px',
-              height: '100px',
-            }}>
+            className={styles.modal}>
             <button type="button" onClick={onClose}>
               x
             </button>
             Modal Content
+            <div style={{ width: '300px' }}></div>
           </div>
         </div>
       )}

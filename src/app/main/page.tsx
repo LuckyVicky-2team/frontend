f3f009e711fa949eae30bebaf873ddf690913f3b
KIMSOOHWAN <ksooh95@gmail.com>
2024-08-01 18:42:22 +0900

    Feat : main css, html

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,10 +1,24 @@
 import DeadLineGather from './_components/DaedLineGather/DeadLineGather';
-import styles from './_components/DaedLineGather/DeadLineGather.module.scss';
+import Image from 'next/image';
+import styles from './main.module.scss';
+import Banner from './img/banner.png';
+import SearchBtn from './img/search.png';
 
 export default function Main() {
   return (
-    <main className={styles.main}>
-      <DeadLineGather />
+    <main>
+      <div className={styles.container}>
+        <div className={styles.banner}>
+          <Image src={Banner} alt="배너이미지" />
+        </div>
+        <div>
+          <input type="text" />
+          <button type="button">
+            <Image src={SearchBtn} alt="검색이미지"></Image>
+          </button>
+        </div>
+        <DeadLineGather />
+      </div>
     </main>
   );
 }

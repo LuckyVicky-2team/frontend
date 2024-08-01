287b0a2eedd25a27f88ca47eaabba7e55f87330a
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-01 15:37:01 +0900

    Feat: TagInput 컴포넌트 생성

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,9 +1,9 @@
 import { forwardRef, InputHTMLAttributes } from 'react';
 import styles from './Input.module.scss';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export default forwardRef<HTMLInputElement, InputProps>(function Input(
+export default forwardRef<HTMLInputElement, IInputProps>(function Input(
   { className, ...props },
   ref
 ) {

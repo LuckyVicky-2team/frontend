61197f95d8d2c8f14ee51ffec1a651c8c5581ba5
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-01 15:37:01 +0900

    Feat: TagInput 컴포넌트 생성

--- src/app/(authentication)/signin/page.tsx
+++ src/app/(authentication)/signin/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import Input from '@/components/common/Input';
+import TagInput from '@/components/common/TagInput';
+import { useState } from 'react';
 
 export default function Signin() {
+  const [items, setItems] = useState<string[]>([]);
+
+  console.log(items);
+
   return (
     <div>
       <Input />
+      <TagInput setItems={setItems} />
     </div>
   );
 }

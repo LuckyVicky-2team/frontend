539ed19da882b64685e146448482018eb2c8f24b
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-01 19:01:26 +0900

    feat: input 에 눈 모양 추가 및 레이아웃 변경

--- src/app/(authentication)/signin/page.tsx
+++ src/app/(authentication)/signin/page.tsx
@@ -1,18 +1,9 @@
-'use client';
-
 import Input from '@/components/common/Input';
-import TagInput from '@/components/common/TagInput';
-import { useState } from 'react';
 
 export default function Signin() {
-  const [items, setItems] = useState<string[]>([]);
-
-  console.log(items);
-
   return (
     <div>
-      <Input />
-      <TagInput setItems={setItems} />
+      <Input isPasswordInput={true} type="password" />
     </div>
   );
 }
--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,25 +1,52 @@
-import { forwardRef, InputHTMLAttributes } from 'react';
+'use client';
+
+import { forwardRef, InputHTMLAttributes, useState } from 'react';
+import Image from 'next/image';
 import { FieldErrors } from 'react-hook-form';
 import styles from './Input.module.scss';
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  inputName?: keyof FormData;
+  fieldName?: keyof FormData;
   errors?: FieldErrors<FormData>;
+  isPasswordInput?: boolean;
 }
 
 export default forwardRef<HTMLInputElement, IInputProps>(function Input(
-  { className, inputName, errors, ...props },
+  { className, type, fieldName, errors, isPasswordInput, ...props },
   ref
 ) {
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+
+  const handleEyeToggle = () => {
+    setPasswordVisible(prev => !prev);
+  };
+
   return (
     <div>
-      <input
-        className={`${styles.input} ${errors && styles.error} ${className}`}
-        ref={ref}
-        {...props}
-      />
-      {errors && inputName && errors[inputName] && (
-        <span className={styles.errorMessage}>{errors[inputName].message}</span>
+      <div className={styles.inputContainer}>
+        <input
+          className={`${styles.input} ${errors && styles.error} ${className}`}
+          type={passwordVisible ? 'text' : type}
+          ref={ref}
+          {...props}
+        />
+        {isPasswordInput && (
+          <button className={styles.eyeButton} onClick={handleEyeToggle}>
+            <Image
+              src={
+                passwordVisible
+                  ? '/assets/icons/openEye.svg'
+                  : '/assets/icons/closedEye.svg'
+              }
+              alt={passwordVisible ? 'password-visible' : 'password-invisible'}
+              width={24}
+              height={24}
+            />
+          </button>
+        )}
+      </div>
+      {errors && fieldName && errors[fieldName] && (
+        <span className={styles.errorMessage}>{errors[fieldName].message}</span>
       )}
     </div>
   );

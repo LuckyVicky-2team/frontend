32799efd2c1239d1e8b23aa0f0138c901b31f0a2
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-02 14:27:24 +0900

    Feat: 로그인/회원가입 로고 클릭시 홈화면 이동

--- src/app/(authentication)/_components/AuthHeader/index.tsx
+++ src/app/(authentication)/_components/AuthHeader/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from './AuthHeader.module.scss';
 
@@ -9,7 +10,16 @@ interface IAuthHeaderProps {
 export default function AuthHeader({ title, text }: IAuthHeaderProps) {
   return (
     <div className={styles.logo}>
-      <Link href="/">
+      <Link href="/" className={styles.pageName}>
+        {hasImage && (
+          <Image
+            src={'/assets/icons/logo.svg'}
+            alt="logo"
+            width={86}
+            height={87}
+            className={styles.logoImage}
+          />
+        )}
         <h1 className={styles.title}>BOGO</h1>
       </Link>
       <h2 className={styles.subTitle}>{title}</h2>

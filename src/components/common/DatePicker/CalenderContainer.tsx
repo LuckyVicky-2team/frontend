669c7c4c277f7a32ee3d3d3c790c3c8a408a122b
Songeun <hannahkeeien@gmail.com>
2024-08-02 14:29:06 +0900

    Chore: react-draft-wysiwyg 설치

--- src/components/common/DatePicker/CalenderContainer.tsx
+++ src/components/common/DatePicker/CalenderContainer.tsx
@@ -1,16 +1,16 @@
-import React, { ReactNode } from 'react';
-import { Portal } from 'react-overlays';
+// import React, { ReactNode } from 'react';
+// import { Portal } from 'react-overlays';
 
-//DatePicker가 모달 밖으로 빠져나갔을 떄 hidden 되는 현상을 막기 위해 만든 컴포넌트 입니다.
+// //DatePicker가 모달 밖으로 빠져나갔을 떄 hidden 되는 현상을 막기 위해 만든 컴포넌트 입니다.
 
-function CalendarContainer({ children }: { children?: ReactNode }) {
-  const el = document.getElementById('calendar-portal');
+// function CalendarContainer({ children }: { children?: ReactNode }) {
+//   const el = document.getElementById('calendar-portal');
 
-  return (
-    <Portal container={el}>
-      <div>{children}</div>
-    </Portal>
-  );
-}
+//   return (
+//     <Portal container={el}>
+//       <div>{children}</div>
+//     </Portal>
+//   );
+// }
 
-export default CalendarContainer;
+// export default CalendarContainer;

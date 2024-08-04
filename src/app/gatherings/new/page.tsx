d9c602857aa48c717c76582ea5ca3de2fb0620be
Songeun <hannahkeeien@gmail.com>
2024-08-04 17:51:53 +0900

    Feat: 라디오 버튼, 인풋 커스터마이징

--- src/app/gatherings/new/page.tsx
+++ src/app/gatherings/new/page.tsx
@@ -25,6 +25,9 @@ interface NewGatheringFormValues {
 export default function NewGatheringPage() {
   const methods = useForm<NewGatheringFormValues>({
     mode: 'all',
+    defaultValues: {
+      type: 'free',
+    },
   });
   const {
     register,
@@ -35,6 +38,7 @@ export default function NewGatheringPage() {
   } = methods;
 
   const [editorVisible, setEditorVisible] = useState(false);
+  const [freeButtonclick, setFreeButtonClick] = useState(true);
 
   const onSubmit = (gatheringInfo: NewGatheringFormValues) => {
     console.log({ gatheringInfo });
@@ -63,15 +67,21 @@ export default function NewGatheringPage() {
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="title">제목</label>
+              {/* input 컴포넌트는 나중에 공통으로 빼겠습니다. */}
               <input
                 id="title"
                 {...register('title', { required: '제목을 입력해 주세요.' })}
+                className={styles.commonInput}
               />
               {errors.title && errors.title.message}
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="tags">태그</label>
-              <input id="tags" {...register('tags')} />
+              <input
+                id="tags"
+                {...register('tags')}
+                className={styles.commonInput}
+              />
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="content">내용</label>
@@ -85,7 +95,11 @@ export default function NewGatheringPage() {
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="location">위치</label>
-              <input id="location" {...register('location')} />
+              <input
+                id="location"
+                {...register('location')}
+                className={styles.commonInput}
+              />
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="gatheringDate">날짜</label>
@@ -116,20 +130,42 @@ export default function NewGatheringPage() {
             </div>
             <div className={styles.inputContainer}>
               <div>모임 유형 선택</div>
-              <label htmlFor="free">자유</label>
+              <label
+                htmlFor="free"
+                className={
+                  freeButtonclick
+                    ? styles.buttonClicked
+                    : styles.buttonNotClicked
+                }
+              />
+              <p>자유</p>
               <input
                 id="free"
                 type="radio"
                 value="free"
                 defaultChecked
                 {...register('type')}
+                onClick={() => {
+                  !freeButtonclick && setFreeButtonClick(true);
+                }}
               />
-              <label htmlFor="accept">수락</label>
+              <label
+                htmlFor="accept"
+                className={
+                  freeButtonclick
+                    ? styles.buttonNotClicked
+                    : styles.buttonClicked
+                }
+              />
+              <p>수락</p>
               <input
                 id="accept"
                 type="radio"
                 value="accept"
                 {...register('type')}
+                onClick={() => {
+                  freeButtonclick && setFreeButtonClick(false);
+                }}
               />
             </div>
           </div>

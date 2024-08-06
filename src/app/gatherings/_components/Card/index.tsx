cbf0834a7642234497a6cbc07224cbe6c1bbfaf5
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-08-06 14:54:55 +0900

    Feat:Card component

---
+++ src/app/gatherings/_components/Card/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import styles from './Card.module.scss';
+import IconButton from '@/components/common/IconButton';
+import Tag from '@/components/common/Tag';
+import Image from 'next/image';
+// import useLocalStorage from '@/hooks/useLocalStorage';
+import useTransText from '../../../../utils/transText';
+
+interface ICardProps {
+  id: number;
+  title: string;
+  tag: string[];
+  participantCount: number;
+  capacity: number;
+  gatheringDate: string;
+  location: string;
+  content: string;
+  image: string;
+  master: { nickName: string };
+  onClick?: (_args: any) => void;
+}
+export default function Card({
+  id,
+  title,
+  tag,
+  participantCount,
+  capacity,
+  gatheringDate,
+  master,
+  location,
+  content,
+  image,
+  onClick,
+}: ICardProps) {
+  const progressValue = (participantCount / capacity) * 100;
+  const { transDate } = useTransText();
+  const { mondthAndDay, time } = transDate(gatheringDate);
+
+  return (
+    <>
+      <div className={styles.card} onClick={onClick}>
+        <div className={styles.thumbnail}>
+          <Image src={image} alt="thumbnail" fill sizes="100%" priority />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <h1>{title} </h1>
+            <h3>&nbsp;{location}</h3>
+            <IconButton
+              size="small"
+              imgUrl={
+                // state && state?.value?.includes(id)
+                // ? '/assets/icons/save.svg':
+                '/assets/icons/unSave.svg'
+              }
+              clickIconButtonHandler={() => {}}
+            />
+          </div>
+
+          <div className={styles.time}>
+            <h3>{mondthAndDay}</h3>
+            <h3 className={styles.timeDetail}>{time}</h3>
+          </div>
+          <div className={styles.tag}>
+            {tag.map((el, idx) => {
+              return (
+                <Tag key={id + idx} backgroundColor="#815489" fontColor="white">
+                  {el}
+                </Tag>
+              );
+            })}
+          </div>
+          <div className={styles.participants}>
+            <progress value={`${progressValue}`} max={'100'}></progress>
+            <span className={styles.count}>
+              {participantCount}/{capacity}
+            </span>
+          </div>
+
+          <p className={styles.description}>{content}</p>
+          <div className={styles.footer}>{master.nickName}</div>
+        </div>
+      </div>
+    </>
+  );
+}

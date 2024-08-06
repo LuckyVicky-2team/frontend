f863762c2a437099e2a75ca32e81263009809e1c
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-07 08:18:02 +0900

    Feat: threadsPage 서버컴포넌트화

--- src/app/threads/page.tsx
+++ src/app/threads/page.tsx
@@ -1,3 +1,29 @@
-export default function Threads() {
-  return;
+import styles from './ThreadsPage.module.scss';
+
+async function getThreads() {
+  const thread = {
+    profile: '/assets/icons/kakao_logo.svg',
+    name: '코드잇',
+    participantsCount: 4,
+    recentMessage: '배고프다',
+    unreadCount: 4,
+  };
+
+  const threads = Array.from({ length: 5 }, (_, i) => {
+    return { ...thread, gatheringId: i };
+  });
+
+  return threads;
+}
+
+export default async function ThreadsPage() {
+  const threads = await getThreads();
+
+  console.log(threads);
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}></div>
+    </div>
+  );
 }

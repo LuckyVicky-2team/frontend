171eafafb6469f58eefc4a559a961a0155c74288
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-07 11:56:41 +0900

    Feat: 작업내용 불러오기

---
+++ src/app/threads/page.tsx
@@ -0,0 +1,46 @@
+import ThreadListItem from './_components/ThreadListItem';
+import styles from './ThreadsPage.module.scss';
+
+async function getThreads() {
+  const thread = {
+    profileImage: '/assets/images/default_profile.png',
+    name: '코드잇',
+    participantsCount: 4,
+    recentMessage: {
+      createdAt: '2024-08-05T17:38:42',
+      contents: '배고파..',
+    },
+    unreadCount: 4,
+  };
+
+  const threads = Array.from({ length: 5 }, (_, i) => {
+    return { ...thread, gatheringId: i };
+  });
+
+  return threads;
+}
+
+export default async function ThreadsPage() {
+  const threads = await getThreads();
+
+  console.log(threads);
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        {threads.map(thread => {
+          return (
+            <ThreadListItem
+              key={thread.gatheringId}
+              profileImage={thread.profileImage}
+              name={thread.name}
+              participantsCount={thread.participantsCount}
+              recentMessage={thread.recentMessage}
+              unreadCount={thread.unreadCount}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}

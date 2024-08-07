21ba828f62d3c4ca696dd9c7ef04b66f07e5a690
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-07 13:21:54 +0900

    Design: 기본 레이아웃 적용

--- src/app/test/yong/page.tsx
+++ src/app/test/yong/page.tsx
@@ -1,12 +1,5 @@
 import KakaoList from '@/components/common/Kakao/KakaoList';
-import styles from './YongTest.module.scss';
 
 export default function YongTest() {
-  return (
-    <div className={styles.background}>
-      <div className={styles.container}>
-        <KakaoList />
-      </div>
-    </div>
-  );
+  return <KakaoList />;
 }

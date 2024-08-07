0d59cc1a19591749c88a9cfc341839bc09bde162
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-07 13:20:47 +0900

    Design: 기본 레이아웃 적용

--- src/app/(authentication)/signup/page.tsx
+++ src/app/(authentication)/signup/page.tsx
@@ -1,4 +1,3 @@
-import BackButton from '@/components/common/BackButton';
 import styles from './Signup.module.scss';
 import AuthHeader from '../_components/AuthHeader';
 import AuthInput from '../_components/AuthInput';
@@ -7,38 +6,33 @@ import Link from 'next/link';
 
 export default function Signup() {
   return (
-    <>
-      <header className={styles.header}>
-        <BackButton />
-      </header>
-      <div className={styles.container}>
-        <AuthHeader
-          text="보고에 회원가입 해주셔서 감사합니다."
-          title="회원가입하기"
+    <div className={styles.container}>
+      <AuthHeader
+        text="보고에 회원가입 해주셔서 감사합니다."
+        title="회원가입하기"
+      />
+      <form className={styles.form}>
+        <AuthInput labelName="이름" />
+        <AuthInput labelName="아이디" type="email" />
+        <AuthInput labelName="닉네임" />
+        <AuthInput
+          labelName="비밀번호"
+          isPasswordInput={true}
+          type="password"
         />
-        <form className={styles.form}>
-          <AuthInput labelName="이름" />
-          <AuthInput labelName="아이디" type="email" />
-          <AuthInput labelName="닉네임" />
-          <AuthInput
-            labelName="비밀번호"
-            isPasswordInput={true}
-            type="password"
-          />
-          <AuthInput
-            labelName="비밀번호 확인"
-            isPasswordInput={true}
-            type="password"
-          />
-          <AuthSubmitButton>확인</AuthSubmitButton>
-        </form>
-        <div className={styles.linkContainer}>
-          이미 회원이신가요?
-          <Link href="/signin" className={styles.loginLink}>
-            로그인
-          </Link>
-        </div>
+        <AuthInput
+          labelName="비밀번호 확인"
+          isPasswordInput={true}
+          type="password"
+        />
+        <AuthSubmitButton>확인</AuthSubmitButton>
+      </form>
+      <div className={styles.linkContainer}>
+        이미 회원이신가요?
+        <Link href="/signin" className={styles.loginLink}>
+          로그인
+        </Link>
       </div>
-    </>
+    </div>
   );
 }

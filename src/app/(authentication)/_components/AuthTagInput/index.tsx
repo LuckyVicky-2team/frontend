a97e4fe9620d76e595dcc8f71061ec6ada5b7592
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-16 11:49:34 +0900

    Feat: PR태그 추천 문구 작성

--- src/app/(authentication)/_components/AuthTagInput/index.tsx
+++ src/app/(authentication)/_components/AuthTagInput/index.tsx
@@ -1,7 +1,7 @@
 import { UseFormSetValue } from 'react-hook-form';
 import TagInput from '@/components/common/TagInput';
-import styles from './AuthTagInput.module.scss';
 import { EmailSignupFormType } from '@/types/request/authRequestTypes';
+import styles from './AuthTagInput.module.scss';
 
 export default function AuthTagInput({
   setValue,
@@ -17,7 +17,28 @@ export default function AuthTagInput({
       <label className={styles.label} htmlFor="prTags">
         PR 태그
       </label>
-      <TagInput id="prTags" setItems={setPRTagValues} />
+      <TagInput
+        id="prTags"
+        setItems={setPRTagValues}
+        placeholder="태그를 작성해주세요!"
+      />
+      <div className={styles.suggest}>
+        <h1 className={styles.title}>이런 태그는 어떠세요?</h1>
+        <ul className={styles.contents}>
+          <li className={styles.content}>• 성별</li>
+          <li className={styles.content}>• 나이</li>
+          <li className={styles.content}>• 성격</li>
+          <li className={styles.content}>• MBTI</li>
+          <li className={styles.content}>• 좋아하는 게임 장르</li>
+          <li className={styles.content}>• 내가 사는 동네</li>
+          <li className={styles.content}>
+            {'• 나의 게임 스타일(전략가, 승부사, 평화주의자)'}
+          </li>
+          <li className={styles.content}>
+            • 그 외 나를 소개할만한 문구를 적어보세요!
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

27d905c06c86d3159e83cebaf241bf029dfdbba5
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-17 17:14:26 +0900

    Fix: 무한 로딩 오류 해결

--- src/api/queryHooks/geolocation.ts
+++ src/api/queryHooks/geolocation.ts
@@ -4,13 +4,18 @@ import { QueryKey } from '../../utils/QueryKey';
 const getCurrentCoordinate = async (): Promise<{ x: number; y: number }> => {
   return new Promise((res, rej) => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        const lat = position.coords.latitude; // 위도
-        const lon = position.coords.longitude; // 경도
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const lat = position.coords.latitude; // 위도
+          const lon = position.coords.longitude; // 경도
 
-        const coordinate = { x: lon, y: lat };
-        res(coordinate);
-      });
+          const coordinate = { x: lon, y: lat };
+          res(coordinate);
+        },
+        () => {
+          rej(new Error('위치 정보 제공이 거부되었습니다.'));
+        }
+      );
     } else {
       rej(new Error('현재 위치를 불러올 수 없습니다.'));
     }

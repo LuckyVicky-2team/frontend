b4563143c26c308b03ff957c1a40a4290c437fd6
Songeun <hannahkeeien@gmail.com>
2024-08-18 14:00:15 +0900

    Feat: sharemodal 따로 분리

--- src/app/gatherings/[id]/_components/GatheringDetails.tsx/index.tsx
+++ src/app/gatherings/[id]/_components/GatheringDetails.tsx/index.tsx
@@ -2,14 +2,12 @@
 import parse from 'html-react-parser';
 import ProfileImages from '../ProfileImages';
 import useModal from '@/hooks/useModal';
-import Modal from '@/components/common/Modal';
-import { handleCopyClipBoard } from '@/utils/handleCopyClipBoard';
-import KakaoShare from '../KakaoShare';
 import Members from '../Members';
 import { useGatheringDetails } from '@/api/queryHooks/gathering';
 import styles from './GatheringDetails.module.scss';
 import { dateTime } from '@/utils/dateTime';
 import Tag from '@/components/common/Tag';
+import ShareModal from '../ShareModal';
 
 interface IGatheringDetailsProps {
   id: number;
@@ -48,6 +46,16 @@ export default function GatheringDetails({ id }: IGatheringDetailsProps) {
         <div className={styles.gatheringInfo}>
           <h1 className={styles.title}>{data.title} 모임원 모집</h1>
           <div>
+            <button type="button" onClick={handleShareModalOpen}>
+              공유하기
+            </button>
+            <ShareModal
+              modalOpen={shareModalOpen}
+              onClose={handleShareModalClose}
+              pathname={pathname}
+            />
+          </div>
+          <div>
             <span>{data.city}</span>
             <span>{data.county}</span>
           </div>
@@ -81,7 +89,8 @@ export default function GatheringDetails({ id }: IGatheringDetailsProps) {
       <div>팀원 목록</div>
       <ProfileImages participants={data.userParticipantResponseList} />
       <button type="button" onClick={handleProfileModalOpen}>
-        참여 멤버
+        참여자 리스트 보기 ({data.totalParticipantCount}/{data.limitParticipant}
+        )
       </button>
       <Members
         modalOpen={profileModalOpen}
@@ -98,31 +107,7 @@ export default function GatheringDetails({ id }: IGatheringDetailsProps) {
           return <div key={i}>{genre}</div>;
         })}
       </div>
-      <div>
-        <button type="button" onClick={handleShareModalOpen}>
-          공유하기
-        </button>
-        <Modal
-          modalOpen={shareModalOpen}
-          onClose={handleShareModalClose}
-          maxWidth={400}
-          full>
-          <div>
-            <KakaoShare path={pathname} likeCount={12} sharedCount={30} />
-            <button type="button">카카오로 공유하기</button>
-            <button
-              type="button"
-              onClick={() =>
-                handleCopyClipBoard(
-                  `${process.env.NEXT_PUBLIC_DEPLOY_URL}${pathname}`
-                )
-              }>
-              클립보드
-              복사하기ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-            </button>
-          </div>
-        </Modal>
-      </div>
+
       <button type="button">(마스터) 내보내기</button>
       <button type="button">(비멤버) 모임 참여하기</button>
       <button type="button">(멤버) 채팅방으로 가기</button>
---
+++ src/app/gatherings/[id]/_components/ShareModal/index.tsx
@@ -0,0 +1,36 @@
+import Modal from '@/components/common/Modal';
+import KakaoShare from '../KakaoShare';
+import { handleCopyClipBoard } from '@/utils/handleCopyClipBoard';
+
+interface IShareModalProps {
+  modalOpen: boolean;
+  onClose: () => void;
+  pathname: string;
+}
+
+export default function ShareModal({
+  modalOpen,
+  onClose,
+  pathname,
+}: IShareModalProps) {
+  return (
+    <div>
+      <Modal modalOpen={modalOpen} onClose={onClose} maxWidth={400} full>
+        <div>
+          <KakaoShare path={pathname} likeCount={12} sharedCount={30} />
+          <button type="button">카카오로 공유하기</button>
+          <button
+            type="button"
+            onClick={() =>
+              handleCopyClipBoard(
+                `${process.env.NEXT_PUBLIC_DEPLOY_URL}${pathname}`
+              )
+            }>
+            클립보드
+            복사하기ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
+}

699d0a6d48ae7afae85401fe618a5d17c1a06605
Songeun <hannahkeeien@gmail.com>
2024-08-19 19:03:32 +0900

    Chore: 파일 이름, 임포트 수정

--- src/components/common/ProfileImage/index.tsx
+++ src/components/common/ProfileImage/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './index.module.scss';
+import styles from './ProfileImage.module.scss';
 import Image from 'next/image';
 
 interface IProfileImageProps {

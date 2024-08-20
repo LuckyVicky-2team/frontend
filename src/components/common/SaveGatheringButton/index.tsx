c6ea700a20e7c6317182ab624680f6950e4bb30f
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-08-20 10:42:42 +0900

    Feat:type에 따른 디자인 분리

--- src/components/common/SaveGatheringButton/index.tsx
+++ src/components/common/SaveGatheringButton/index.tsx
@@ -4,10 +4,27 @@ import { useSaveItemState } from '@/hooks/useSavedItemsStatus';
 interface ISaveGatheringButtonProps {
   id: number;
   size?: 'xsmall' | 'small' | 'medium' | 'large';
+  type?: 'blue' | 'red' | 'default';
   className?: string;
 }
+const path = '/assets/icons';
+const iconPathsObj = {
+  blue: {
+    saved: `${path}/blueSave.svg`,
+    unsaved: `${path}/unSave.svg`,
+  },
+  red: {
+    saved: `${path}/redSave.svg`,
+    unsaved: `${path}/unSave.svg`,
+  },
+  default: {
+    saved: `${path}/defaultSave.svg`,
+    unsaved: `${path}/defaultUnSave.svg`,
+  },
+};
 
 export default function SaveGatheringButton({
+  type = 'default',
   id,
   size,
   className,
@@ -19,12 +36,14 @@ export default function SaveGatheringButton({
   };
 
   const isSaved = savedItem?.includes(id);
-
+  const imgUrl = isSaved
+    ? iconPathsObj[type].saved
+    : iconPathsObj[type].unsaved;
   return (
     <IconButton
       className={className}
       size={size}
-      imgUrl={isSaved ? '/assets/icons/save.svg' : '/assets/icons/unSave.svg'}
+      imgUrl={imgUrl}
       clickIconButtonHandler={handleButton}
     />
   );

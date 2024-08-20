a5bd0491fe2fe63966e42619156b194f07742a0f
Songeun <hannahkeeien@gmail.com>
2024-08-20 16:55:48 +0900

    Design: 모임 상세 Footer ui

--- src/app/gatherings/[id]/_components/Footer/index.tsx
+++ src/app/gatherings/[id]/_components/Footer/index.tsx
@@ -7,8 +7,6 @@ import { useRouter } from 'next/navigation';
 
 interface IGatheringFooterProps {
   id: number;
-  // isSaved: boolean;
-  // setSaveItem: (_newValue: number) => void;
   type: string | undefined;
 }
 
@@ -16,34 +14,39 @@ export default function GatheringFooter({ id, type }: IGatheringFooterProps) {
   const router = useRouter();
   return (
     <div className={styles.background}>
-      <div>
-        <button
-          className={styles.backButton}
-          onClick={() => {
-            router.back;
-          }}>
+      <button
+        className={styles.backButton}
+        onClick={() => {
+          router.back();
+        }}>
+        <Image
+          src={'/assets/icons/chevron-left.svg'}
+          alt="수정 이미지"
+          width={36}
+          height={36}
+        />
+      </button>
+      <div className={styles.cta}>
+        {!type && '모임 참가하기'}
+        {(type === 'LEADER' || type === 'PARTICIPANT') && '채팅방으로 가기'}
+      </div>
+      {type !== 'LEADER' ? (
+        <SaveGatheringButton
+          id={id}
+          type="red"
+          className={`${styles.zzimButton}`}
+          rectangle
+        />
+      ) : (
+        <div className={styles.editButton}>
           <Image
             src={'/assets/icons/pen.svg'}
             alt="수정 이미지"
             width={36}
             height={36}
           />
-        </button>
-        <div className={styles.cta}>
-          {!type && '모임 참가하기'}
-          {(type === 'LEADER' || type === 'PARTICIPANT') && '채팅방으로 가기'}
         </div>
-        {type !== 'LEADER' ? (
-          <SaveGatheringButton
-            id={id}
-            type="red"
-            className={`${styles.zzimButton}`}
-            rectangle
-          />
-        ) : (
-          <div className={styles.editButton}></div>
-        )}
-      </div>
+      )}
     </div>
   );
 }

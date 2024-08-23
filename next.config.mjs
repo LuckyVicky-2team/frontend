2cbc34b98c198ae24fd6e7c31f41248f0b736053
ksooh95 <142282880+ksooh95@users.noreply.github.com>
2024-08-23 15:35:05 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN
-      ? [
-          {
-            protocol: 'https',
-            hostname: process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN,
-            port: '', // 포트가 필요한 경우 추가
-          },
-        ]
-      : [], // 환경 변수가 없으면 빈 배열로 설정
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd248qe8akqy587.cloudfront.net',
+        port: '',
+      },
+    ],
   },
   async rewrites() {
      return [

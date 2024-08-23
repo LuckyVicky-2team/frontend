f2f996c12e26c0159bdc0855f22c8428fa3ec62c
ksooh95 <142282880+ksooh95@users.noreply.github.com>
2024-08-23 15:07:58 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ${process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN}
+    remotePatterns: process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN
       ? [
           {
             protocol: 'https',
-            hostname: `${process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN}`,
-            port: '',
+            hostname: process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN,
+            port: '', // 포트가 필요한 경우 추가
           },
         ]
-      : [],
+      : [], // 환경 변수가 없으면 빈 배열로 설정
   },
   async rewrites() {
-    return [
+     return [
       {
         source: '/api/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,

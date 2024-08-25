c04a1045a7f6cfc37cd8d80fa31aed75f7205599
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-25 18:11:28 +0900

    Feat: MannerBar 애니메이션 추가

--- src/app/other-profile/[id]/_components/MannerBar/index.tsx
+++ src/app/other-profile/[id]/_components/MannerBar/index.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import styles from './MannerBar.module.scss';
 
 interface IMannerBarProps {
@@ -5,15 +8,41 @@ interface IMannerBarProps {
 }
 
 export default function MannerBar({ rating }: IMannerBarProps) {
+  const [progress, setProgress] = useState(0); // progress-width
+  const [position, setPosition] = useState(0); // rating-position
+  const [ratingValue, setRatingValue] = useState(0); // rating-text
+
+  useEffect(() => {
+    setProgress(rating * 20);
+    setPosition(rating * 14);
+
+    let start = 0;
+    const end = rating;
+    const duration = 1000; // 애니메이션 길이
+    const stepTime = 50; // 프레임 간격
+    const stepValue = (end - start) / (duration / stepTime);
+
+    const timer = setInterval(() => {
+      start += stepValue;
+      if (start >= end) {
+        start = end;
+        clearInterval(timer);
+      }
+      setRatingValue(parseFloat(start.toFixed(1)));
+    }, stepTime);
+
+    return () => clearInterval(timer);
+  }, [rating]);
+
   return (
     <div className={styles.container}>
       <div className={styles.progressBar}>
         <div
           className={styles.progress}
-          style={{ width: `${rating * 20}%` }}></div>
+          style={{ width: `${progress}%` }}></div>
       </div>
-      <div className={styles.rating} style={{ left: `${rating * 14}%` }}>
-        {rating}
+      <div className={styles.rating} style={{ left: `${position}%` }}>
+        {ratingValue}
       </div>
     </div>
   );

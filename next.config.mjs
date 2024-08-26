6d3f5d0dc1bd70474e00782a9810fae320cd45c3
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-08-26 17:20:51 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd248qe8akqy587.cloudfront.net',
+        port: '',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;

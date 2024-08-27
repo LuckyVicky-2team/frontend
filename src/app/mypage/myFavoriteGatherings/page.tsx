2e3e2742cc360d3288cd7fec9b78b62fd7c01d8d
Jin-Chanyong <cksdyd324@gmail.com>
2024-08-27 15:08:46 +0900

    Feat: 카드 클릭 시 개별 모임 페이지로 이동

--- src/app/mypage/myFavoriteGatherings/page.tsx
+++ src/app/mypage/myFavoriteGatherings/page.tsx
@@ -5,8 +5,9 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import SaveGatheringButton from '@/components/common/SaveGatheringButton';
 import { IWishListItemProps } from '@/types/response/WishListRES';
-import styles from './myFavoriteGatherings.module.scss';
 import { dateToISOString, transDate } from '@/utils/common';
+import Link from 'next/link';
+import styles from './myFavoriteGatherings.module.scss';
 
 export default function MyFavoriteGatherings() {
   const [favoriteGatherings, setFavoriteGatherings] = useState<
@@ -42,59 +43,61 @@ export default function MyFavoriteGatherings() {
 
             return (
               <div className={styles.myFavoriteGatheringItem} key={e.meetingId}>
-                <div className={styles.thumbnail}>
-                  <Image
-                    fill
-                    src={
-                      e.thumbnail
-                        ? `https://${process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN}/${e.thumbnail}`
-                        : '/assets/mainImages/game.png'
-                    }
-                    alt="찜목록 리스트"
-                    style={{ objectFit: 'cover' }}
-                    unoptimized
-                    onError={e =>
-                      (e.currentTarget.src = '/assets/mainImages/game.png')
-                    }
-                  />
-                  {processedCurrentDate.mondthAndDay ===
-                    processedGatheringDate.mondthAndDay && (
-                    <div className={styles.deadLine}>
-                      <Image
-                        src="/assets/icons/clock.svg"
-                        alt="dead-line"
-                        width={15}
-                        height={15}
-                      />
-                      {`오늘 ${processedGatheringDate.time.split(':')[0]}시 마감`}
-                    </div>
-                  )}
-                  <div
-                    className={
-                      styles.participant
-                    }>{`${e.currentParticipant}/${e.limitParticipant}`}</div>
-                </div>
-                <div className={styles.mid}>
-                  <Image
-                    src="/assets/icons/mapPin.svg"
-                    alt="place"
-                    width={18}
-                    height={18}
-                    className={styles.mapPin}
-                  />
-                  <p>{e.locationName}</p>
-                  <SaveGatheringButton
-                    id={e.meetingId}
-                    size="medium"
-                    type="blue"
-                    className={styles.heart}
-                  />
-                </div>
-                <div className={styles.title2}>{e.title}</div>
-                <div className={styles.date}>
-                  <p>{processedGatheringDate.mondthAndDay}</p>
-                  <p>{processedGatheringDate.time}</p>
-                </div>
+                <Link href={`/gatherings/${e.meetingId}`}>
+                  <div className={styles.thumbnail}>
+                    <Image
+                      fill
+                      src={
+                        e.thumbnail
+                          ? `https://${process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN}/${e.thumbnail}`
+                          : '/assets/mainImages/game.png'
+                      }
+                      alt="찜목록 리스트"
+                      style={{ objectFit: 'cover' }}
+                      unoptimized
+                      onError={e =>
+                        (e.currentTarget.src = '/assets/mainImages/game.png')
+                      }
+                    />
+                    {processedCurrentDate.mondthAndDay ===
+                      processedGatheringDate.mondthAndDay && (
+                      <div className={styles.deadLine}>
+                        <Image
+                          src="/assets/icons/clock.svg"
+                          alt="dead-line"
+                          width={15}
+                          height={15}
+                        />
+                        {`오늘 ${processedGatheringDate.time.split(':')[0]}시 마감`}
+                      </div>
+                    )}
+                    <div
+                      className={
+                        styles.participant
+                      }>{`${e.currentParticipant}/${e.limitParticipant}`}</div>
+                  </div>
+                  <div className={styles.mid}>
+                    <Image
+                      src="/assets/icons/mapPin.svg"
+                      alt="place"
+                      width={18}
+                      height={18}
+                      className={styles.mapPin}
+                    />
+                    <p>{e.locationName}</p>
+                    <SaveGatheringButton
+                      id={e.meetingId}
+                      size="medium"
+                      type="blue"
+                      className={styles.heart}
+                    />
+                  </div>
+                  <div className={styles.title2}>{e.title}</div>
+                  <div className={styles.date}>
+                    <p>{processedGatheringDate.mondthAndDay}</p>
+                    <p>{processedGatheringDate.time}</p>
+                  </div>
+                </Link>
               </div>
             );
           })

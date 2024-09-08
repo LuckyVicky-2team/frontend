1357bd2fb2b8d4865809d43e9508d8f8520f3028
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-09-08 15:12:34 +0900

    Style: 모임 목록 scrollbar, 반응형 재수정, skeleton, selectBox ui 수정

--- src/components/common/SelectBox/index.tsx
+++ src/components/common/SelectBox/index.tsx
@@ -1,4 +1,12 @@
-import React, { ChangeEvent } from 'react';
+'use client';
+
+import React, {
+  ChangeEvent,
+  useState,
+  ReactNode,
+  FocusEvent,
+  useEffect,
+} from 'react';
 import styles from './SelectBox.module.scss';
 
 interface ISelectOptionProps {
@@ -9,11 +17,15 @@ interface ISelectBoxProps extends ISelectOptionProps {
   className?: string;
   isDisabled?: boolean;
   value?: string;
+  leftLabel?: ReactNode;
+  rightLabel?: ReactNode;
   clickOptionHandler: (_event: ChangeEvent<HTMLSelectElement>) => void;
   id: string;
 }
 
 export default function SelectBox({
+  leftLabel,
+  rightLabel,
   value,
   isDisabled = false,
   className,
@@ -22,31 +34,86 @@ export default function SelectBox({
   optionSet = [],
   id = '',
 }: ISelectBoxProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedName, setSelectedName] = useState(value);
+
+  const handleToggle = () => {
+    if (!isDisabled) {
+      setIsOpen(prev => !prev);
+    }
+  };
+
+  const handleOptionClick = (optionValue: any, optionName: string) => {
+    setSelectedName(optionName);
+    setIsOpen(false);
+    clickOptionHandler({
+      target: { value: optionValue },
+    } as unknown as ChangeEvent<HTMLSelectElement>);
+  };
+
+  const handleBlur = (event: FocusEvent<HTMLDivElement>) => {
+    if (!event.currentTarget.contains(event.relatedTarget as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const noOptionTitle =
+      !optionTitle && value === undefined && optionSet.length > 0;
+    if (noOptionTitle) {
+      const firstOption =
+        typeof optionSet[0] === 'object' && 'name' in optionSet[0]
+          ? optionSet[0].name
+          : optionSet[0];
+      setSelectedName(firstOption); // 첫 번째 인덱스 값을 자동선택
+    } else if (optionTitle && (value === '' || value === undefined)) {
+      setSelectedName(optionTitle);
+    }
+  }, [value]);
+
   return (
-    <select
+    <div
+      className={`${styles.filter} ${className}`}
       id={id}
-      value={value}
-      disabled={isDisabled}
-      className={`${styles.select} ${className}`}
-      onChange={clickOptionHandler}>
-      {optionTitle && <option value={''}>{optionTitle}</option>}
-      {optionSet.map((item, idx) => {
-        if (typeof item === 'object') {
-          return (
-            <option
-              key={`${item.name ?? item.value}_${idx}`}
-              value={item?.value ?? item.name}>
-              {item.name}
-            </option>
-          );
-        } else {
-          return (
-            <option key={idx} value={item}>
-              {item}
-            </option>
-          );
-        }
-      })}
-    </select>
+      tabIndex={0}
+      onBlur={handleBlur}>
+      <div
+        className={`${styles.select} ${isDisabled ? styles.disabled : ''}`}
+        onClick={handleToggle}>
+        {leftLabel && (
+          <span className={`${styles.label} ${styles.left}`}>{leftLabel}</span>
+        )}
+        {selectedName || optionTitle}
+        {rightLabel && (
+          <span className={`${styles.label} ${styles.right}`}>
+            {rightLabel}
+          </span>
+        )}
+      </div>
+      {isOpen && (
+        <ul className={styles.options}>
+          {optionTitle && (
+            <li onClick={() => handleOptionClick('', '')}>{optionTitle}</li>
+          )}
+          {optionSet.map((item, idx) => {
+            const optionValue =
+              typeof item === 'object' ? (item?.value ?? item.name) : item;
+            const optionName = typeof item === 'object' ? item.name : item;
+            return (
+              <>
+                <li
+                  key={`${optionName}_${idx}`}
+                  onClick={() => handleOptionClick(optionValue, optionName)}
+                  className={
+                    selectedName === optionName ? styles.selected : ''
+                  }>
+                  {optionName}
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      )}
+    </div>
   );
 }

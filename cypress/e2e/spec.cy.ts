2163b3ca59d9ae3529f47872e1cfdc67a200c0fb
Songeun <hannahkeeien@gmail.com>
2024-09-25 15:25:09 +0900

    Test: cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.codeit.kr');
+  beforeEach('passes', () => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should contain "App Router" text', () => {
+    cy.contains('App Router');
   });
 });

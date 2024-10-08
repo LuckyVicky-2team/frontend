a31513965802b60ce2b3ba52d8027125c5d6fc04
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-10-08 17:45:59 +0900

    Revert "Feat:SEO -sitemap"
    This reverts commit a8d619d427ba0da313de0f6d4250139a327df1fa.

--- src/app/test/won/page.tsx
+++ src/app/test/won/page.tsx
@@ -1,8 +1,3 @@
-import sitemap from '@/app/sitemap';
-import { useEffect } from 'react';
 export default function TestPage() {
-  useEffect(() => {
-    sitemap();
-  }, []);
-  return <></>;
+  return <>test</>;
 }

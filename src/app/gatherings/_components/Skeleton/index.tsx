8ecea5dd96ddd6dd9d0f781e82a0f9683df1bbea
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-10-10 15:41:26 +0900

    Feat: data값 null 로 들어왔을경우 spinner 처리, skeleton 사용자 사용감 개선  처리

--- src/app/gatherings/_components/Skeleton/index.tsx
+++ src/app/gatherings/_components/Skeleton/index.tsx
@@ -1,9 +1,18 @@
 import styles from './Skeleton.module.scss';
 
-export default function Skeleton() {
+interface ISkeletonProps {
+  type: 'default' | 'review';
+}
+
+export default function Skeleton({ type = 'default' }: ISkeletonProps) {
   return (
-    <div className={styles.skeletonCard}>
-      <div className={styles.thumbnail} />
+    <div
+      className={styles.skeletonCard}
+      style={{ transform: `${type === 'review' && 'translateY(-10%)'}` }}>
+      <div
+        className={styles.thumbnail}
+        style={{ scale: `${type === 'review' && '0.8'}` }}
+      />
       <div className={styles.content}>
         <div className={styles.header}>
           <div className={styles.info}>
@@ -14,20 +23,24 @@ export default function Skeleton() {
               <div className={styles.timeDetail} />
             </div>
           </div>
-          <div className={styles.iconButton} />
+          {type === 'default' && <div className={styles.iconButton} />}
         </div>
-        <div className={styles.tagContainer}>
-          <div className={styles.tag} />
-          <div className={styles.tag} />
-        </div>
-        <div className={styles.gameContainer}>
-          <div className={styles.game} />
-          <div className={styles.game} />
-        </div>
-        <div className={styles.participants}>
-          <div className={styles.participantDetails} />
-          <div className={styles.progress} />
-        </div>
+        {type === 'default' && (
+          <>
+            <div className={styles.tagContainer}>
+              <div className={styles.tag} />
+              <div className={styles.tag} />
+            </div>
+            <div className={styles.gameContainer}>
+              <div className={styles.game} />
+              <div className={styles.game} />
+            </div>
+            <div className={styles.participants}>
+              <div className={styles.participantDetails} />
+              <div className={styles.progress} />
+            </div>
+          </>
+        )}
         <div className={styles.seeDetail} />
       </div>
     </div>

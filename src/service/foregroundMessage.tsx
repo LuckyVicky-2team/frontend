eac4af7e09dbd983095a935e569956b35dc00011
Songeun <hannahkeeien@gmail.com>
2024-10-24 14:15:02 +0900

    Fix: 빌드 오류 해결 (cypress에서는 fcm을 테스트하지 못함. 브라우저 지원 X)

--- src/service/foregroundMessage.tsx
+++ src/service/foregroundMessage.tsx
@@ -13,38 +13,39 @@ import { useEffect } from 'react';
 //   };
 // }
 
-const messaging = getMessaging(app);
-
 //포그라운드 상태일 때 알림 설정
 export function ForegroundMessage() {
   const router = useRouter();
 
   useEffect(() => {
-    const foregroundMessage = onMessage(messaging, payload => {
-      // console.log("알림 도착 ", payload);
-      const notificationTitle = payload.notification?.title;
-      const notificationOptions = {
-        body: payload.notification?.body,
-        data: { url: payload.fcmOptions?.link },
-      };
-
-      if (Notification.permission === 'granted' && notificationTitle) {
-        const notification = new Notification(
-          notificationTitle,
-          notificationOptions
-        );
-
-        notification.onclick = event => {
-          event.preventDefault();
-          if (payload.fcmOptions?.link) {
-            const url = new URL(payload.fcmOptions?.link);
-            router.push(url.pathname + url.search);
-          }
+    if (app) {
+      const messaging = getMessaging(app);
+      const foregroundMessage = onMessage(messaging, payload => {
+        // console.log("알림 도착 ", payload);
+        const notificationTitle = payload.notification?.title;
+        const notificationOptions = {
+          body: payload.notification?.body,
+          data: { url: payload.fcmOptions?.link },
         };
-      }
-    });
 
-    return () => foregroundMessage();
+        if (Notification.permission === 'granted' && notificationTitle) {
+          const notification = new Notification(
+            notificationTitle,
+            notificationOptions
+          );
+
+          notification.onclick = event => {
+            event.preventDefault();
+            if (payload.fcmOptions?.link) {
+              const url = new URL(payload.fcmOptions?.link);
+              router.push(url.pathname + url.search);
+            }
+          };
+        }
+      });
+
+      return () => foregroundMessage();
+    }
   }, [router]);
 
   return null;

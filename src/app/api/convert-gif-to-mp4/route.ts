65e036b8c7208cf709184356b9da3525038484e2
Songeun <hannahkeeien@gmail.com>
2024-11-12 16:26:47 +0900

    Feat: LCP 이미지 최적화

--- src/app/api/convert-gif-to-mp4/route.ts
+++ src/app/api/convert-gif-to-mp4/route.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import fs2 from 'fs/promises';
 import path from 'path';
 import { NextResponse } from 'next/server';
+import sharp from 'sharp';
 
 // 환경 변수로부터 FFmpeg 경로 가져오기
 const ffmpegPath = process.env.FFMPEG_PATH
@@ -42,43 +43,56 @@ export async function POST(request: any) {
     // 파일 확장자 확인
     const extension = url.split('.').pop().toLowerCase();
 
-    // GIF 파일이 아닌 경우 400 응답 반환
-    if (extension !== 'gif') {
-      return new NextResponse('Only GIF files can be converted to MP4', {
-        status: 400,
-      });
-    }
-
     // 경로 설정 (프로젝트 디렉토리 내에 tmp 폴더 사용)
     const tempDir = path.resolve(process.cwd(), 'tmp');
     await fs2.mkdir(tempDir, { recursive: true });
     const gifPath = path.join(tempDir, 'input.gif');
     const mp4Path = path.join(tempDir, 'output.mp4');
 
-    // GIF 파일을 로컬로 다운로드
+    // 파일을 로컬로 다운로드
     const gifResponse = await fetch(url);
     const gifBuffer = await gifResponse.arrayBuffer();
+    const mediaBuffer = Buffer.from(gifBuffer);
     fs.writeFileSync(gifPath, Buffer.from(gifBuffer));
 
-    // GIF를 MP4로 변환
-    await new Promise((resolve, reject) => {
-      ffmpeg(gifPath)
-        .output(mp4Path)
-        .on('end', resolve)
-        .on('error', reject)
-        .run();
-    });
+    let optimizedBuffer;
 
-    // MP4 파일을 읽고 반환
-    const mp4Buffer = fs.readFileSync(mp4Path);
+    // GIF 파일이 아닌 경우 400 응답 반환
+    if (extension !== 'gif') {
+      // return new NextResponse('Only GIF files can be converted to MP4', {
+      //   status: 400,
+      // });
+      // 이미지 압축
+      optimizedBuffer = await sharp(mediaBuffer)
+        .resize({ width: 400 })
+        .avif({ quality: 50 }) // JPEG 포맷, 품질 30%
+        .toBuffer();
+    }
+    if (extension === 'gif') {
+      // GIF를 MP4로 변환
+      await new Promise((resolve, reject) => {
+        ffmpeg(gifPath)
+          .output(mp4Path)
+          .on('end', resolve)
+          .on('error', reject)
+          .run();
+      });
 
-    // 임시 파일 삭제
-    await safeUnlink(gifPath);
-    await safeUnlink(mp4Path);
+      // MP4 파일을 읽고 반환
+      optimizedBuffer = fs.readFileSync(mp4Path);
 
-    return new NextResponse(mp4Buffer, {
+      // 임시 파일 삭제
+      await safeUnlink(gifPath);
+      await safeUnlink(mp4Path);
+    }
+    // return new NextResponse(optimizedBuffer, {
+    //   headers: {
+    //     'Content-Type': 'video/mp4',
+    //   },
+    // });
+    return new NextResponse(optimizedBuffer, {
       headers: {
-        'Content-Type': 'video/mp4',
+        'Content-Type': extension === 'gif' ? 'video/mp4' : 'image/jpeg',
       },
     });
   } catch (error) {
--- src/utils/gifToMp4.tsx
+++ src/utils/gifToMp4.tsx
@@ -2,10 +2,14 @@
 
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import styles from './gifToMp4.module.scss';
+// import styles from './gifToMp4.module.scss';
 
 const GifToMp4 = ({ url }: { url: string }) => {
-  const [mp4Url, setMp4Url] = useState<string | null>(null);
+  // const [mp4Url, setMp4Url] = useState<string | null>(null);
+  const [optimizedMediaUrl, setOptimizedMediaUrl] = useState<string | null>(
+    null
+  );
+  const [isVideo, setIsVideo] = useState(false);
 
   useEffect(() => {
     const convertGifToMp4 = async () => {
@@ -15,16 +19,19 @@ const GifToMp4 = ({ url }: { url: string }) => {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ url }),
         });
-        const mp4Blob = await response.blob();
-        const mp4Url = URL.createObjectURL(mp4Blob);
-        setMp4Url(mp4Url);
-
-        if (!response.ok) {
-          // 400 에러 확인
-          if (response.status === 400) {
-            setMp4Url('notGif');
-          }
-        }
+        // const mp4Blob = await response.blob();
+        // const mp4Url = URL.createObjectURL(mp4Blob);
+        // setMp4Url(mp4Url);
+        const blob = await response.blob();
+        const mediaUrl = URL.createObjectURL(blob);
+        setIsVideo(response.headers.get('Content-Type') === 'video/mp4');
+        setOptimizedMediaUrl(mediaUrl);
+        // if (!response.ok) {
+        //   // 400 에러 확인
+        //   if (response.status === 400) {
+        //     setMp4Url('notGif');
+        //   }
+        // }
       } catch (error) {
         alert('파일을 불러오는 중 오류가 발생했습니다.');
       }
@@ -35,26 +42,31 @@ const GifToMp4 = ({ url }: { url: string }) => {
     }
   }, [url]);
 
-  if (mp4Url === 'notGif')
+  if (isVideo === false && optimizedMediaUrl)
     return (
       <Image
-        src={url}
+        src={optimizedMediaUrl}
         alt="썸네일 이미지"
         priority
         fill
         objectFit="contain"
         quality={50}
-        loading="eager"
-        onError={() => setMp4Url('/assets/images/detail-image-default.png')}
+        onError={() =>
+          setOptimizedMediaUrl('/assets/images/detail-image-default.png')
+        }
       />
     );
 
   // if (!mp4Url) return <p>Converting...</p>;
-  if (!mp4Url) return;
+  if (!optimizedMediaUrl) return;
 
   return (
-    <video controls autoPlay loop className={styles.video}>
-      <source src={mp4Url} type="video/mp4" />
+    <video
+      controls
+      autoPlay
+      loop
+      style={{ width: '100%', height: '100%', objectFit: 'contain' }}>
+      <source src={optimizedMediaUrl} type="video/mp4" />
     </video>
   );
 };

c3eb6cb11ef86f12686713d4748f8b7a704d6966
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-11-13 17:37:41 +0900

    Feat: 이미지 포맷 자동변경 처리

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',

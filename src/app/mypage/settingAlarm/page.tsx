cbac99123f6107e50f83cdc79132f561ce539252
Soo <ksooh95@gmail.com>
2024-11-15 16:03:24 +0900

    Feat : 알림설정목록api 적용

--- src/app/mypage/settingAlarm/page.tsx
+++ src/app/mypage/settingAlarm/page.tsx
@@ -1,21 +1,69 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getNotification } from '@/api/apis/mypageApis';
 import styles from './settingAlarm.module.scss';
+
+interface INotificationProps {
+  additionalContent: 'string';
+  content: 'string';
+  isAgreed: 'boolean';
+  messageType: 'string';
+}
 
 export default function SettingAlarm() {
   const [alarmOn, setAlarmOn] = useState<boolean>(true);
+  const [alrmList, setAlrmList] = useState<INotificationProps[]>([]);
 
   const handleAlarm = () => {
     setAlarmOn(!alarmOn);
   };
+
+  const fetchNotification = async () => {
+    try {
+      const res = await getNotification();
+      setAlrmList(res.data);
+    } catch (error) {
+      console.error('Failed to fetch notifications:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotification();
+  }, []);
+
+  console.log(alrmList);
 
   return (
     <div className={styles.relative}>
       <h1 className={styles.title}>알림 설정</h1>
       <div className={styles.setWrap}>
         <h2>기본 알림</h2>
-        <div className={styles.settingItem}>
+        {alrmList?.map((item, i) => {
+          return (
+            <div className={styles.settingItem} key={i}>
+              <h3>{item?.content}</h3>
+              <div className={styles.settingBtn}>
+                <div className={styles.btnWrap}>
+                  <button
+                    type={'button'}
+                    onClick={() => {
+                      handleAlarm();
+                    }}>
+                    <p>ON</p>
+                    <p>OFF</p>
+                    <span
+                      className={
+                        alarmOn === false ? `${styles.alarmOff}` : ''
+                      }></span>
+                  </button>
+                </div>
+              </div>
+              <div className={styles.addCon}>{item?.additionalContent}</div>
+            </div>
+          );
+        })}
+        {/* <div className={styles.settingItem}>
           <h3>알람이 올 상황</h3>
           <div className={styles.settingBtn}>
             <div className={styles.btnWrap}>
@@ -33,7 +81,7 @@ export default function SettingAlarm() {
               </button>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );

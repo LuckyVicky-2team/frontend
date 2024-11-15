a751d169fc8ae74b1c2ffb0714b33b307e90f3a5
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-15 15:36:32 +0900

    Fix: 리프레시 토큰 로직 수정

--- src/actions/AuthActions.ts
+++ src/actions/AuthActions.ts
@@ -1,25 +1,32 @@
 'use server';
 
-import { postReissueAccessToken } from '@/api/apis/authApis';
 import { cookies } from 'next/headers';
 
-/**
- * 쿠키의 리프레쉬 토큰을 통해 AccessToken 을 재발급하여 반환.
- * @returns string | undefind
- */
-export const getTokenFromCookie = async () => {
-  const refreshToken = cookies().get('Authorization');
+// /**
+//  * 쿠키의 리프레쉬 토큰을 통해 AccessToken 을 재발급하여 반환.
+//  * @returns string | undefind
+//  */
+// export const getTokenFromCookie = async () => {
+//   const refreshToken = cookies().get('Authorization');
 
-  if (!refreshToken) return;
+//   if (!refreshToken) return;
 
-  let accessToken;
+//   let accessToken;
 
-  try {
-    const response = await postReissueAccessToken(refreshToken.value);
-    accessToken = response.headers['authorization'];
-  } catch (error) {
-    return;
-  }
+//   try {
+//     const response = await postReissueAccessToken(refreshToken.value);
+//     accessToken = response.headers['authorization'];
+//   } catch (error) {
+//     return;
+//   }
 
-  return accessToken;
+//   return accessToken;
+// };
+
+export const deleteRefreshToken = () => {
+  cookies().delete('Authorization');
+};
+
+export const checkRefreshToken = () => {
+  return !!cookies().get('Authorization');
 };

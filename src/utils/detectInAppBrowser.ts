5aec633af375882c922fdfc212c1d717ce58d51a
Songeun <hannahkeeien@gmail.com>
2024-11-19 13:36:59 +0900

    Fix: suppressHydrationWarning

--- src/utils/detectInAppBrowser.ts
+++ src/utils/detectInAppBrowser.ts
@@ -1,25 +1,23 @@
 export const detectInAppBrowser = (agent: any) => {
-  if (typeof window !== 'undefined') {
-    const inappRegex = [
-      /KAKAOTALK/i,
-      /Instagram/i,
-      /NAVER/i,
-      /zumapp/i,
-      /Whale/i,
-      /Snapchat/i,
-      /Line/i,
-      /everytimeApp/i,
-      /WhatsApp/i,
-      /Electron/i,
-      /wadiz/i,
-      /AliApp/i,
-      /FB_IAB/i,
-      /FBAAN/i,
-      /FBAV/i,
-      /FBIOS/i,
-      /FBSS/i,
-      /SamsungBrowser/i,
-    ];
-    return inappRegex.some(mobile => agent.match(mobile));
-  }
+  const inappRegex = [
+    /KAKAOTALK/i,
+    /Instagram/i,
+    /NAVER/i,
+    /zumapp/i,
+    /Whale/i,
+    /Snapchat/i,
+    /Line/i,
+    /everytimeApp/i,
+    /WhatsApp/i,
+    /Electron/i,
+    /wadiz/i,
+    /AliApp/i,
+    /FB_IAB/i,
+    /FBAAN/i,
+    /FBAV/i,
+    /FBIOS/i,
+    /FBSS/i,
+    /SamsungBrowser/i,
+  ];
+  return inappRegex.some(mobile => agent.match(mobile));
 };

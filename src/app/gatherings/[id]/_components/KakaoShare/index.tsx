7137decbfe953f07c81e4cc619b1a821cb03e2d6
Songeun <hannahkeeien@gmail.com>
2024-11-22 15:04:17 +0900

    Fix: 카카오톡 공유 이미지 선명하게 변경

--- src/app/gatherings/[id]/_components/KakaoShare/index.tsx
+++ src/app/gatherings/[id]/_components/KakaoShare/index.tsx
@@ -40,7 +40,7 @@ export default function KakaoShare({
       content: {
         title: 'BOGO',
         description: '보드게임 하러 오세요! BOGO',
-        imageUrl: 'https://i.ibb.co/7rMNL32/logo.png', // 큰 이미지
+        imageUrl: 'https://i.ibb.co/SJNMVwB/Logo-1.png', // 큰 이미지
         link: {
           mobileWebUrl: realUrl,
           webUrl: realUrl,

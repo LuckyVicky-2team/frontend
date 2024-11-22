e3c2b106e60e1dd1d160b8ac510c09072939f79e
Songeun <hannahkeeien@gmail.com>
2024-11-22 14:41:49 +0900

    Fix: 아이폰 크롬 브라우저에서 모임 상세 푸터 여백 방지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,10 @@ export default function RootLayout({
           sizes="180x180"
           href="/assets/icons/logo.svg"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
       </head>
       <body className={inter.className}>
         <ForegroundMessage />

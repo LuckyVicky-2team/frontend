bc75fc2d5537f4b262eae38de85967852c018164
Songeun <hannahkeeien@gmail.com>
2024-11-22 14:27:59 +0900

    Fix: 로그아웃 상태일 때 meApi 요청 취소 (모임 상세)

--- src/api/queryHooks/me.ts
+++ src/api/queryHooks/me.ts
@@ -3,8 +3,18 @@ import { useQuery } from '@tanstack/react-query';
 import { getMe } from '../apis/meApi';
 
 export const useMe = () => {
-  return useQuery({
+  const hasAuth = Boolean(localStorage.getItem('accessToken'));
+
+  const { data, isError, isPending, isLoading } = useQuery({
     queryKey: [QueryKey.USER.ME],
     queryFn: getMe,
+    enabled: hasAuth, // accessToken이 없을 때만 쿼리 실행
   });
+
+  return {
+    data: hasAuth ? data : null,
+    isError,
+    isPending: hasAuth ? isPending : false,
+    isLoading: hasAuth ? isLoading : false,
+  };
 };

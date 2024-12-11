b01b695664d715a55d74ace37a7ea09ce7864f7e
Jin-Chanyong <cksdyd324@gmail.com>
2024-12-11 14:34:09 +0900

    Fix: 로그아웃 api 오류 수정

--- src/api/apis/logOutApis.ts
+++ src/api/apis/logOutApis.ts
@@ -3,8 +3,8 @@
 import { cookies } from 'next/headers';
 
 export async function logout() {
-  cookies().delete('Authorization');
-  cookies().delete({
+  await cookies().delete('Authorization');
+  await cookies().delete({
     name: 'Authorization',
     domain: '.board-go.net',
     path: '/',
